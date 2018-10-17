02a8eab794d981f61bc4b008c4c01fd5df721386
Ronaldo Tomazeli Duarte <ronaldo.tomazeli@gmail.com>
2018-10-17 00:33:27 -0300

    Add decoders for AND instruction

--- rocknes/decoder.py
+++ rocknes/decoder.py
@@ -5,6 +5,20 @@ class InstructionDecoder():
     def __init__(self, cpu):
         self.cpu = cpu
 
+    def decode_and_immediate(self):
+        operand = self.cpu.memory_read(self.cpu.reg_pc + 1)
+        return (2, operand)
+
+    def decode_and_zero_page(self):
+        operand_address = self.cpu.memory_read(self.cpu.reg_pc + 1)
+        return (2, operand_address)
+
+    def decode_and_absolute(self):
+        lsb = self.cpu.memory_read(self.cpu.reg_pc + 1)
+        msb = self.cpu.memory_read(self.cpu.reg_pc + 2)
+        operand_address = address_from_little_endian(lsb, msb)
+        return (3, operand_address)
+
     def decode_jmp(self):
         lsb = self.cpu.memory_read(self.cpu.reg_pc + 1)
         msb = self.cpu.memory_read(self.cpu.reg_pc + 2)
--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -1,6 +1,33 @@
 from unittest import mock
 
 
+@mock.patch('rocknes.cpu.Cpu.memory_read', return_value=0xcc)
+def test_decode_and_immediate(mock_memory_read, cpu):
+    length, operand = cpu.decoder.decode_and_immediate()
+
+    assert mock_memory_read.call_count == 1
+    assert length == 2
+    assert operand == 0xcc
+
+
+@mock.patch('rocknes.cpu.Cpu.memory_read', return_value=0xcc)
+def test_decode_and_zero_page(mock_memory_read, cpu):
+    length, operand_address = cpu.decoder.decode_and_zero_page()
+
+    assert mock_memory_read.call_count == 1
+    assert length == 2
+    assert operand_address == 0xcc
+
+
+@mock.patch('rocknes.cpu.Cpu.memory_read', side_effect=[0xcc, 0xdd])
+def test_decode_and_absolute(mock_memory_read, cpu):
+    length, operand_address = cpu.decoder.decode_and_absolute()
+
+    assert mock_memory_read.call_count == 2
+    assert length == 3
+    assert operand_address == 0xddcc
+
+
 @mock.patch('rocknes.cpu.Cpu.memory_read', side_effect=[0xad, 0xde])
 def test_decode_jmp(mock_memory_read, cpu):
     length, address = cpu.decoder.decode_jmp()
