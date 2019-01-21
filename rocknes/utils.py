cec1794cd20cc7aa2779965d358f7b4555101aad
Ronaldo Tomazeli Duarte <ronaldo.tomazeli@gmail.com>
2019-01-20 22:53:32 -0200

    Add function for testing page boundaries

--- rocknes/utils.py
+++ rocknes/utils.py
@@ -1,2 +1,6 @@
 def address_from_little_endian(lsb, msb):
     return (msb << 8) | lsb
+
+
+def addresses_on_same_page(address_1, address_2):
+    return address_1 & 0xff00 == address_2 & 0xff00
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,13 @@
-from rocknes.utils import address_from_little_endian
+from rocknes.utils import address_from_little_endian, addresses_on_same_page
 
 
 def test_address_from_little_endian():
     assert address_from_little_endian(0xee, 0xff) == 0xffee
+
+
+def test_addresses_same_page():
+    assert addresses_on_same_page(0x2dc, 0x2ff) is True
+
+
+def test_addresses_different_page():
+    assert addresses_on_same_page(0x2dc, 0x7dc) is False
